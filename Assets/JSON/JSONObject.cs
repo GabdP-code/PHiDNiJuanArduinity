5b59ce1a3690dc8dc5d8a11f4176221f9919a59f
lineignacio <beebug310@gmail.com>
2018-10-10 00:33:02 +0800

    delete comment

--- Assets/JSON/JSONObject.cs
+++ Assets/JSON/JSONObject.cs
@@ -1,7 +1,5 @@
-#define PRETTY		//Comment out when you no longer need to read JSON to disable pretty Print system-wide
-//Using doubles will cause errors in VectorTemplates.cs; Unity speaks floats
-#define USEFLOAT	//Use floats for numbers instead of doubles	(enable if you're getting too many significant digits in string output)
-//#define POOLING	//Currently using a build setting for this one (also it's experimental)
+#define PRETTY
+#define USEFLOAT
 
 #if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
 using UnityEngine;
@@ -11,12 +9,6 @@ using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
-/*
- * http://www.opensource.org/licenses/lgpl-2.1.php
- * JSONObject class v.1.4.1
- * for use with Unity
- * Copyright Matt Schoen 2010 - 2013
- */
 
 public class JSONObject {
 #if POOLING
